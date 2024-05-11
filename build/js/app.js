0bf9a12ae39213f83903a0e2fce20adea0ba35a6
Hussain022 <hussainahmed.vu@gmail.com>
2024-05-11 16:32:57 +0600

    portfolio first commit

---
+++ build/js/app.js
@@ -0,0 +1,34 @@
+const navToggleBtn = document.getElementById('nav-toggle-btn');
+const navList = document.getElementById('nav-list');
+const navListItem = document.querySelectorAll('#nav-list > li > a');
+
+
+
+navToggleBtn.addEventListener('click', () => {
+  navList.classList.toggle('hidden');  
+  navList.classList.toggle('block');  
+});
+
+navList.addEventListener('click', () => {
+  navList.classList.toggle('hidden');  
+  navList.classList.toggle('block');  
+});
+
+const navObj = {
+    hero: 0,
+    about: 1,
+    skills: 2,
+    projects: 3,
+    contact: 4
+};
+
+const navObserver = new IntersectionObserver(entries => {
+  entries.forEach((entry) => {
+    navListItem[navObj[entry.target.id]].classList.toggle('md:after:bg-green-500', entry.isIntersecting);
+  })
+}, {
+  threshold: 0.5
+})
+
+const intersectingItems = document.querySelectorAll('.intersecting-class');
+intersectingItems.forEach(el => navObserver.observe(el))
